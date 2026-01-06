aa29a6fae21b173042286535054bdc417ac06877
christinerabine <153837791+christinerabine@users.noreply.github.com>
2026-01-06 19:22:39 +0000

    Update siteProps with new user information and enhance social icon styles with bounce animation

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Christine Rabine",
+  title: "UX Designer & Front-End Developer",
+  email: "christine.rabine@gmail.com",
+  gitHub: "christinerabine",
+  instagram: "christine.krysalis",
+  linkedIn: "christinerabine",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "crabine",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
